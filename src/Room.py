9e8e958eccac62189a0dba0b5db137f66f6905fa
oisinosullivan9 <oisinosull@gmail.com>
2023-05-04 16:48:02 +0100

    made test for room class to check that meeting room can only be booked when its raining

--- src/Room.py
+++ src/Room.py
@@ -1,4 +1,14 @@
 class Room:
-    room_number = None
-    def __init__(self, room_number, driver):
-        self.room_number = room_number
+    #room_number = None
+    #def __init__(self, room_number, driver):
+     #   self.room_number = room_number
+
+    def __init__(self):
+        self.is_booked = False
+
+    def book_room(self, is_raining):
+        if is_raining:
+            self.is_booked = True
+            return True
+        else:
+            return False
---
+++ tests/test_RoomTest.py
@@ -0,0 +1,22 @@
+import unittest
+from room import Room
+
+class test_RoomTest(unittest.TestCase):
+    def test_book_room(self):
+        room = Room()
+
+        # Test booking room when it's raining
+        is_raining = True
+        result = room.book_room(is_raining)
+        self.assertTrue(result, "The room should be booked when it's raining.")
+        self.assertTrue(room.is_booked, "The room should be marked as booked when it's raining.")
+
+        # Test booking room when it's not raining
+        room.is_booked = False
+        is_raining = False
+        result = room.book_room(is_raining)
+        self.assertFalse(result, "The room shoudn't be booked when it's not raining.")
+        self.assertFalse(room.is_booked, "The room should not be marked as booked when it's not raining.")
+
+if __name__ == '__main__':
+    unittest.main()
