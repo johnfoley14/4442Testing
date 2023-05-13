e1137332d794e6e231f569fcdf214f1b24f3a996
JohnFoley <johnmfoley2003@yahoo.com>
2023-05-13 16:55:31 +0100

    Constructors for booking and room classes

--- src/Room.py
+++ src/Room.py
@@ -1,4 +1,16 @@
+from Booking import *
+
 class Room:
-    room_number = None
-    def __init__(self, room_number, driver):
-        self.room_number = room_number
+
+    def __init__(self, room_id, room_name, room_type,  capacity, location):
+        self.room_id = room_id
+        self.room_name = room_name
+        self.room_type = room_type
+        self.capacity = capacity
+        self.location = location
+
+
+    def bookRoom(self, user_id, start_time, end_time):
+        if self.isAvailable():# Create a booking for this room
+            new_booking = Booking(user_id, self.room_id, start_time, end_time)
+            return new_booking
