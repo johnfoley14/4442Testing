3f833bc41a1bd38a60c9daa0e3603cd39351db9b
Eoghan <eoghano4321@gmail.com>
2023-05-15 18:41:07 +0100

    Migrated functionality into relevant classes

--- main/src/Booking.py
+++ main/src/Booking.py
@@ -5,7 +5,7 @@ sys.path.insert(0, './')
 
 conn = None
 host="localhost"
-database="iser"
+database="postgres"
 user="postgres"
 password="root"
 port = 5432
@@ -18,11 +18,22 @@ class Booking:
         self.start_time = start_time
         self.end_time = end_time
 
-    def getPrice(self):
-        # Calculate the price based on the room rate and duration of booking
-        duration = self.end_time - self.start_time
-        hours = duration.total_seconds() / 3600
-        return self.room.rate * hours
+    
+    def createBooking(self, id):
+        connection = psycopg2.connect(
+                host= host,
+                database= database,
+                user= user,
+                password=password,
+                port = port)
+        cur = connection.cursor()
+        
+        cur.execute('select max(bookingid) from bookings')
+        id = int(cur.fetchone()[0]) + 1
+        cur.execute('insert into bookings (bookingid, roomid, userid, starttime, endtime) values (%s, %s, %s, %s, %s)', (id, self.room_id, self.user_id, self.start_time, self.end_time))
+        connection.commit()
+        cur.close()
+        connection.close()
 
     def isAvailable(self):
         connection = psycopg2.connect(host= host, database= database, user= user, password=password, port = port)
@@ -43,6 +54,28 @@ class Booking:
         cur = connection.cursor()
         cur.execute("SELECT * FROM BOOKINGS WHERE room_id = %s AND start_time <= %s AND end_time >= %s",
                     (self.room_id, self.end_time, self.start_time))
+     
+    def get_all_bookings(self):
+        data = []
+        connection = psycopg2.connect(
+            host= host,
+            database= database,
+            user= user,
+            password=password,
+            port = port)
+        cur = connection.cursor()
+        data.clear()
+        cur.execute('select * from bookings')
+        for row in cur.fetchall():
+            cur.execute('select roomname from rooms where roomid = %s', (row[1],))
+            rname = cur.fetchone()[0]
+            cur.execute('select username from users where userid = %s', (row[2],))
+            uname = cur.fetchone()[0]
+            data.append({"bookingid": str(row[0]), "roomname":rname, "username":uname, "starttime": row[3], "endtime": row[4]})
+        cur.close()
+        connection.close()
+        
+        return data
         
     # def isAvailable(self):
     #     connection = psycopg2.connect(user=user, password=password, dsn=conn_string)
