237f00b61c64dab44512cb304919a3efb4838432
Eoghan <eoghano4321@gmail.com>
2023-05-15 14:02:14 +0100

    Added functionality to create bookings from rooms page. Need to clean up htmls and migrate functionality to correct files

--- src/Booking.py
+++ src/Booking.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from main import *
+import psycopg2
+import os
+#import main
 
 class Booking:
     def __init__(self, user_id, room_id, start_time, end_time):
@@ -14,15 +16,15 @@ class Booking:
         hours = duration.total_seconds() / 3600
         return self.room.rate * hours
 
-    def isAvailable(self):
-        connection = oracledb.connect(user=user, password=password, dsn=conn_string)
-        # Check if there is a conflicting booking for this room and time
-        cur = connection.cursor()
-        cur.execute("SELECT * FROM BOOKINGS WHERE room_id = %s AND start_time <= %s AND end_time >= %s",
-                    (self.room_id, self.end_time, self.start_time))
-        conflicting_bookings = cur.fetchall()
-        cur.close()
-        if len(conflicting_bookings) > 0:
-            return False
-        else:
-            return True
+    # def isAvailable(self):
+    #     connection = psycopg2.connect(user=user, password=password, dsn=conn_string)
+    #     # Check if there is a conflicting booking for this room and time
+    #     cur = connection.cursor()
+    #     cur.execute("SELECT * FROM BOOKINGS WHERE room_id = %s AND start_time <= %s AND end_time >= %s",
+    #                 (self.room_id, self.end_time, self.start_time))
+    #     conflicting_bookings = cur.fetchall()
+    #     cur.close()
+    #     if len(conflicting_bookings) > 0:
+    #         return False
+    #     else:
+    #         return True
